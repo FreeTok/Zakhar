5d69d7ee5e3c3489b00bb770d50dc00fd8958e0a
ArtemonPavlov <freetokdevelop@gmail.com>
2023-02-22 19:23:30 +0300

    1

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
+
+[Serializable]
+public struct img
+{
+    public Sprite image;
+    public VideoClip video;
+
+    public void SetEl(Image imageToSet, VideoPlayer videoImg, RectTransform rt)
+    {
+        if (video)
+        {
+            if (imageToSet.gameObject.activeSelf) imageToSet.gameObject.SetActive(false);
+            if (!videoImg.gameObject.activeSelf) videoImg.gameObject.SetActive(true);
+
+            videoImg.clip = video;
+            videoImg.isLooping = true;
+            videoImg.Play();
+        }
+
+        else
+        {
+            if (!imageToSet.gameObject.activeSelf) imageToSet.gameObject.SetActive(true);
+            if (videoImg.gameObject.activeSelf) videoImg.gameObject.SetActive(false);
+
+            imageToSet.sprite = image;
+
+            rt.sizeDelta = new Vector2(image.rect.width, image.rect.height);
+        }
+    }
+}
+
 
 public class UIManager : MonoBehaviour
 {
-    public Sprite[] images;
+    public img[] images;
     public Image img;
+    public VideoPlayer videoImg;
 
     private int activeImage;
     private RectTransform rt;
 
     private void Awake()
     {
+        print(img.gameObject.activeSelf);
+        print(videoImg.gameObject.activeSelf);
+
         rt = img.GetComponent(typeof(RectTransform)) as RectTransform;
         SetElement(0);
     }
@@ -29,6 +66,7 @@ public class UIManager : MonoBehaviour
 
     private void SetElement(int i)
     {
+        print(i);
         if (activeImage + i < 0)
         {
             activeImage = images.Length - activeImage + i;
@@ -44,7 +82,6 @@ public class UIManager : MonoBehaviour
             activeImage += i;
         }
 
-        img.sprite = images[activeImage];
-        rt.sizeDelta = new Vector2(images[activeImage].rect.width, images[activeImage].rect.height);
+        images[activeImage].SetEl(img, videoImg, rt);
     }
 }
